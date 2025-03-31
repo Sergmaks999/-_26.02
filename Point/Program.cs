6588109f0a81632ebe38c5ca2a39de30bcd99fd1
User <User@WIN-7RSVVMUHAJG>
2025-03-31 12:47:15 +0500

    Добавьте файлы проекта.

---
+++ Point/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public void MoveBy(double dx, double dy, double dz)
+    {
+        X += dx;
+        Y += dy;
+        Z += dz;
+    }
+
+    public override string ToString()
+    {
+        return $"X: {X}, Y: {Y}, Z: {Z}";
+    }
+
+    public static void Main(string[] args)
+    {
+        Point myPoint = new Point(1.0, 2.0, 3.0);
+        Console.WriteLine("точка: " + myPoint);
+
+        myPoint.MoveBy(2.5, -1.0, 0.5);
+        Console.WriteLine("Точка после перемещения: " + myPoint);
+    }
+}
