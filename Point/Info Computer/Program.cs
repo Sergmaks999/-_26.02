d81bdddcbe40ea77dc0c5945e0103fc8a3b9f02b
maks <sergeevmaks444@gmail.com>
2025-03-31 13:23:14 +0500

    Add files via upload

---
+++ Point/Info Computer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class PersonalComputer
+{
+    public double CpuClockSpeed { get; set; }
+    public int RamSize { get; set; }
+    public int HardDriveSize { get; set; }
+
+    public PersonalComputer(double cpuClockSpeed, int ramSize, int hardDriveSize)
+    {
+        CpuClockSpeed = cpuClockSpeed;
+        RamSize = ramSize;
+        HardDriveSize = hardDriveSize;
+    }
+
+    public string Info()
+    {
+        return $"Тактовая частота процессора: {CpuClockSpeed} GHz, Объем оперативной памяти: {RamSize} GB, Объем жесткого диска: {HardDriveSize} GB";
+    }
+
+    public override string ToString()
+    {
+        return Info();
+    }
+
+    public static void Main(string[] args)
+    {
+        PersonalComputer myComputer = new PersonalComputer(2.8, 8, 1000);
+        Console.WriteLine(myComputer.Info());
+    }
+}
